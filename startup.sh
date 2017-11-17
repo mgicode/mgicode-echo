a7100712c1c23ffbc4793776924be95d9a1c1dcc
pengrk <546711211@qq.com>
2017-11-17 23:12:40 +0800

    init

--- startup.sh
+++ startup.sh
@@ -1,29 +1,26 @@
-  export POD_IP=`/sbin/ifconfig -a | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print \$2}' | tr -d "addr" `
-  echo " ############POD_IP     $POD_IP"
+#  export POD_IP=`/sbin/ifconfig -a | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print \$2}' | tr -d "addr" `
+#  echo " ############POD_IP     $POD_IP"
+#   serverPort=""
+#   if [ $HTTP_PORT ]; then
+#    serverPort=" --server.port=${HTTP_PORT}"
+#   else
+#    serverPort=""
+#   fi
+#
+#   tcpPort=""
+#   if [ $TCP_PORT ]; then
+#    tcpPort=" --tcp.port=${TCP_PORT}"
+#   else
+#    tcpPort=""
+#   fi
+#
+#   serverName=""
+#   if [ $SERVER_NAME ]; then
+#    serverName=" --server.name=${SERVER_NAME}"
+#   else
+#    serverName=""
+#   fi
 
-   serverPort=""
-   if [ $HTTP_PORT ]; then
-    serverPort=" --server.port=${HTTP_PORT}"
-   else
-    serverPort=""
-   fi
+ #echo  "TCP_PORT:${TCP_PORT},SERVER_NAME:${SERVER_NAME},HTTP_PORT:${HTTP_PORT}"
 
-   tcpPort=""
-   if [ $TCP_PORT ]; then
-    tcpPort=" --tcp.port=${TCP_PORT}"
-   else
-    tcpPort=""
-   fi
-
-   serverName=""
-   if [ $SERVER_NAME ]; then
-    serverName=" --server.name=${SERVER_NAME}"
-   else
-    serverName=""
-   fi
-
-   #TCP_PORT  SERVER_NAME  HTTP_PORT
-   echo  "TCP_PORT:${TCP_PORT},SERVER_NAME:${SERVER_NAME},HTTP_PORT:${HTTP_PORT}"
-
-  cd /
   java -jar -Xms256m  -Xmx512m  /mgicode-echo-1.0-SNAPSHOT.jar  ${serverName}  ${serverPort}   ${tcpPort}
